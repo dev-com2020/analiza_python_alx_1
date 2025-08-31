fdb9bdee901101d0e324a6aee18efe8afab494cb
DEV-COM <kwp@tlen.pl>
2025-08-31 11:56:54 +0200

    Dodano funkcję dodawania dwóch liczb oraz przykładowe użycie w skrypcie startowym.

---
+++ program/dodaj.py
@@ -0,0 +1,6 @@
+def dodawanie(a, b):
+    """
+    Funkcja do dodawania dwóch liczb.
+    Zwraca sumę dwóch liczb.
+    """
+    return a + b
---
+++ start.py
@@ -0,0 +1,5 @@
+from program import dodaj as d
+import datetime as dt
+
+print(d.dodawanie(5, 10))
+print(dt.datetime.now())
